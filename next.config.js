852265ea71941d70c2a1dbfccf626e041fd8fcc6
andfel17 <andfel17@gmail.com>
2021-02-05 14:38:57 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,9 +6,12 @@ const nextConfig = {
         domains: ['i.imgur.com','*.tmdb.org','www.themoviedb.org']
     },
     env: {
-        NAME: '',
-        URL: '',
-        APIURL: ''
+        NAME: 'AnimeON',
+        URL: 'http://www.localhost/',
+        APIURL: 'http://localhost/public/api/',
+        STREAMURL: 'https://api.animelatinohd.com/stream/',
+        GA_TRACKING_ID: 'UA-59047120-3',
+        DISQUS_SHORTNAME: 'animeonmoe'
     },
 }
 
