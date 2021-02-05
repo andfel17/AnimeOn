5e74377ae21ee9abc20fff081b4b2641f1e2e251
andfel17 <andfel17@gmail.com>
2021-02-05 14:20:21 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,12 +6,9 @@ const nextConfig = {
         domains: ['i.imgur.com','*.tmdb.org','www.themoviedb.org']
     },
     env: {
-        NAME: 'AnimeLHD',
-        URL: 'https://www.animelatinohd.com/',
+        NAME: '',
+        URL: '',
         APIURL: '',
-        STREAMURL: 'https://api.animelatinohd.com/stream/',
-        GA_TRACKING_ID: 'UA-162278791-1',
-        DISQUS_SHORTNAME: 'animelatinohd'
     },
 }
 
